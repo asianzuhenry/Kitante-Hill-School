1d29bbce0335980e820c0736aaf817a5ca0f531a
Asianzu Henry <asianzuhenry@gmail.com>
2024-03-30 09:52:13 +0300

    My First Commit
    This is my first commit

---
+++ js/script.js
@@ -0,0 +1,42 @@
+// This is for the javascript
+
+
+
+const homePage = () => {
+    var aboutSection = document.getElementById("aboutsection").style.display = "none"
+    let heroSection = document.getElementById("herosection").style.display = "block"
+
+    // heroSection.style.backgoundColor = "red"
+
+
+}
+
+
+const aboutPage = () => {
+    var aboutSection = document.getElementById("aboutsection").style.display = "block"
+    var heroSection = document.getElementById("herosection").style.display = "none"
+    let contactSection = document.getElementById("contactsection").style.display = "none"
+
+
+    // const aboutSectionIndicator = document.getElementsByClassName(".menu li a")
+
+    // aboutSection
+
+    // aboutSectionIndicator.classList = "indicator"
+
+}
+
+const contactPage = () => {
+    var aboutSection = document.getElementById("aboutsection").style.display = "none"
+    let heroSection = document.getElementById("herosection").style.display = "none"
+    let contactSection = document.getElementById("contactsection").style.display = "block"
+
+
+    // heroSection.style.backgoundColor = "red"
+
+
+}
+
+
+
+
